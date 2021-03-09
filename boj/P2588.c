62d2ddd4e09e49fce5b012462bbeb566aec69e12
wizd100 <wizd100@gmail.com>
2021-03-10 01:15:46 +0900

    init

---
+++ boj/P2588.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    int num2;
+
+    scanf("%d%d", &num1, &num2);
+    
+    printf("%d\n", num1 * (num2 % 10));
+    printf("%d\n", num1 * ((num2 % 100) / 10));
+    printf("%d\n", num1 * (num2 / 100));
+    printf("%d\n", num1 * num2);
+    return 0;
+}
